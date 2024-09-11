7f526ac0a4043b5955798fa9f1cf4c94f9defaba
sushil pandey <susilpanday9@gmail.com>
2024-09-11 10:30:13 +0545

    helo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  cost [Gamepad, setGames] =useState([
+    {id:1,name:"Game1", price:10},
+    {
+      id:2,name:"Volleyball", price:20
+    },
+    {
+      id:3,name:"football", price:20
+    },{
+      id:4,name:"cricket", price:20
+    },
+  ])
+  return (
+    <div className='bg-slate-400'>This is index Page</div>
+  )
+}
+
+export default App
